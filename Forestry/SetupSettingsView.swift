b7fe0da590df5212d6e338257c00d7b6bd1a8014
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-12-02 02:09:15 +0530

    Settings stuff

---
+++ Forestry/SetupSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SetupSettingsView.swift
+//  Forestry
+//
+//  Created by Shrish Deshpande on 02/12/23.
+//
+
+import SwiftUI
+
+struct SetupSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetupSettingsView()
+}
