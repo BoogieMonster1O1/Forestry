2e24244ac375eef258f6a305d8759e976a24aee0
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-12-02 06:53:27 +0530

    more ml stuff

---
+++ Forestry/IncidentReportSheet.swift
@@ -0,0 +1,18 @@
+//
+//  IncidentReportSheet.swift
+//  Forestry
+//
+//  Created by Shrish Deshpande on 02/12/23.
+//
+
+import SwiftUI
+
+struct IncidentReportSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IncidentReportSheet()
+}
